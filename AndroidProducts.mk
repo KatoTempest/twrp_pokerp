df3691c4cb5cf5a9c9abe5b67e85c63b4d9144e9
GarfieldFernando <ferchito.zoma@gmail.com>
2021-02-12 22:55:11 +0000

    pokerp: added missing lunch to AndroidProducst.mk
    Signed-off-by: GarfieldFernando <ferchito.zoma@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+#
+# LITCH ES MACACO #
+#
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_pokerp.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_pokerp-user \
+    omni_pokerp-userdebug \
+    omni_pokerp-eng
