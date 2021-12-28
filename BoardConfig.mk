4a47ef505c5b21a771db39546ce541c8647921a0
KatoTempest <reftlight309@gmail.com>
2021-12-28 12:43:01 -0600

    FDE test 6

---
+++ BoardConfig.mk
@@ -0,0 +1,92 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/pokerp
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := pokerp
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432 # This is the maximum known partition size, but it can be higher, so we just omit it
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_BOOTIMG_HEADER_VERSION := 1
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_RAMDISK_OFFSET := 0x11a88000
+BOARD_KERNEL_TAGS_OFFSET := 0x07808000
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+#TARGET_KERNEL_SOURCE := kernel/motorola/pokerp
+#TARGET_KERNEL_CONFIG := pokerp_defconfig
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6765
+
+# TWRP Configuration
+TW_THEME := portrait_hdpi
+TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_USE_TOOLBOX := true
+TW_Y_OFFSET := 53
+TW_H_OFFSET := -53
+TW_NO_SCREEN_BLANK := true
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+TARGET_USES_MKE2FS := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_SCREEN_BLANK_ON_BOOT := true
+
+TW_RECOVERY_ADDITIONAL_RELINK_FILES += \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw/android.hardware.keymaster@3.0-impl.so \
+    $(TARGET_OUT_VENDOR_EXECUTABLES)/hw/android.hardware.keymaster@3.0-service \
+    $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libkeymaster3device.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+
+
+# Hack to get keymaster to recognize the key files
+PLATFORM_SECURITY_PATCH := 2021-08-01
+VENDOR_SECURITY_PATCH := 2019-10-01
+PLATFORM_VERSION := 9.0.0
---
+++ vendorsetup.sh
@@ -0,0 +1,90 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2020 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="pokerpatas"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+   	export TW_DEFAULT_LANGUAGE="en"
+	export OF_SCREEN_H="2340"
+    export OF_STATUS_H="83"
+    export OF_STATUS_INDENT_LEFT="52"
+    export OF_STATUS_INDENT_RIGHT="52"
+	export OF_HIDE_NOTCH="1"
+	#export OF_NO_SPLASH_CHANGE="1"
+	export FOX_USE_XZ_UTILS="1"
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+	export OF_USE_SYSTEM_FINGERPRINT="1"
+
+	export FOX_USE_NANO_EDITOR="1"
+	export FOX_USE_TAR_BINARY="1"
+	export FOX_USE_ZIP_BINARY="1"
+	export OF_USE_NEW_MAGISKBOOT="1"
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+	export OF_VANILLA_BUILD="1"
+	export OF_DISABLE_UPDATEZIP="1"
+	export OF_FLASHLIGHT_ENABLE="0"
+	export OF_FLASHLIGHT_ENABLE="0"
+	export OF_TWRP_COMPATIBILITY_MODE="1"
+	export OF_NO_SAMSUNG_SPECIAL"1"
+
+	#Android 12 gsi support (experimental)
+	#export OF_SKIP_DECRYPTED_ADOPTED_STORAGE="1"
+
+	#Build type and mantainer info
+	export OF_MAINTAINER="KatoTempest"
+    export FOX_BUILD_TYPE="Unofficial"
+	export OF_MAINTAINER_AVATAR="$PWD/device/motorola/pokerpatas/avatar.png"
+
+
+	# OTA for custom ROMs
+        #export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES="1"
+        #export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR="1"
+
+        # -- add settings for R11 --
+        export OF_USE_TWRP_SAR_DETECT="1"
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
+        export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+        # -- end R11 settings --
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+
+	add_lunch_combo omni_"$FDEVICE"-eng
+	add_lunch_combo omni_"$FDEVICE"-userdebug
+fi
+#
