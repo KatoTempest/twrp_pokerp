ca834ec05ac2affbaaba6c06a33e2c033bedd820
KatoTempest <reftlight309@gmail.com>
2022-01-02 23:05:17 -0600

    pokerp: test-moved blobs to correct location

--- omni_pokerp.mk
+++ omni_pokerp.mk
@@ -5,20 +5,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Inherit from pokerp device
 $(call inherit-product, device/motorola/pokerp/device.mk)
-
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pokerp
 PRODUCT_NAME := omni_pokerp
@@ -26,37 +17,6 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto e(6) plus
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=pokerp \
-    BUILD_PRODUCT=pokerp \
-    TARGET_DEVICE=pokerp
-
-# Properties for decryption
-#PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.keystore=mt6765 \
-    ro.hardware.gatekeeper=mt6765 \
-    ro.build.system_root_image=true
-
-# enable stock zip packages flash
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.treble.enabled=true \
-    persist.sys.usb.config=mtp \
-    persist.service.adb.enable=1 \
-    persist.service.debuggable=1 \
-    ro.secure=1 \
-    ro.adb.secure=0 \
-    ro.allow.mock.location=0
-
-
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
-
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-    ro.bootimage.build.date.utc \
-    ro.build.date.utc
-
-# Dimen
-TARGET_SCREEN_HEIGHT := 1560
-TARGET_SCREEN_WIDTH := 720
-TW_THEME := portrait_hdpi
