ddaae616a2122357576a51f276d58e81a9271134
KatoTempest <reftlight309@gmail.com>
2022-01-28 01:02:01 -0600

    pokerp: modified decryption stuffs

---
+++ omni_pokerp.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from pokerp device
+$(call inherit-product, device/motorola/pokerp/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := pokerp
+PRODUCT_NAME := omni_pokerp
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto e(6) plus
+PRODUCT_MANUFACTURER := motorola
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.bootimage.build.date.utc \
+    ro.build.date.utc
+
+#PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=mt6765 \
+    ro.hardware.gatekeeper=mt6765
+
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="full_p161bn-user 9 PTBS29.401-58-8 58-8 release-keys"
+
+BUILD_FINGERPRINT := motorola/pokerp_64/pokerp:9/PTBS29.401-58-8/58-8:user/release-keys
