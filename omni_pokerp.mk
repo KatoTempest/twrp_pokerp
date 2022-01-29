dc96178e1c777b64ce20df9b746c1b8dd7bdfda7
KatoTempest <reftlight309@gmail.com>
2022-01-29 12:41:59 -0600

    pokerp: fbe disabled, because the kernel does not fully support that encryption method

--- omni_pokerp.mk
+++ omni_pokerp.mk
@@ -27,7 +27,7 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
 
-#PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.bootimage.build.date.utc \
     ro.build.date.utc
 
